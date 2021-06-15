8961ceade84058ea1b19c3fbf0b6c643e3f6109f
github-action-benchmark <github@users.noreply.github.com>
2021-06-15 22:15:46 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for ea5120ff2b263d04ea7154b24e2e9e7bd225748a

---
+++ dev/bench/data.js
@@ -0,0 +1,35 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1623795345451,
+  "repoUrl": "https://github.com/jinglinpeng/dataprep",
+  "entries": {
+    "DataPrep.EDA Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "name": "jinglinpeng",
+            "username": "jinglinpeng"
+          },
+          "committer": {
+            "name": "jinglinpeng",
+            "username": "jinglinpeng"
+          },
+          "id": "ea5120ff2b263d04ea7154b24e2e9e7bd225748a",
+          "message": "workflows",
+          "timestamp": "2021-06-15T21:35:11Z",
+          "url": "https://github.com/jinglinpeng/dataprep/pull/2/commits/ea5120ff2b263d04ea7154b24e2e9e7bd225748a"
+        },
+        "date": 1623795343973,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.16174027948335315,
+            "unit": "iter/sec",
+            "range": "stddev: 0.06038567742975435",
+            "extra": "mean: 6.18275177459999 sec\nrounds: 5"
+          }
+        ]
+      }
+    ]
+  }
+}
