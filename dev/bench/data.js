93010d5ab91ca7adb004c794426a3562e0bc866f
github-action-benchmark <github@users.noreply.github.com>
2021-07-13 18:42:45 +0000

    add DataPrep.EDA Benchmarks (pytest) benchmark result for 3bb7d6f7ad9b3a95fa0ac3730a4b68a7b76efe3f

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1623796288745,
+  "lastUpdate": 1626201765391,
   "repoUrl": "https://github.com/jinglinpeng/dataprep",
   "entries": {
     "DataPrep.EDA Benchmarks": [
@@ -81,6 +81,37 @@ window.BENCHMARK_DATA = {
             "unit": "iter/sec",
             "range": "stddev: 0.10201845761024664",
             "extra": "mean: 6.221681000399997 sec\nrounds: 5"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "jlpengcs@gmail.com",
+            "name": "Jinglin Peng",
+            "username": "jinglinpeng"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3bb7d6f7ad9b3a95fa0ac3730a4b68a7b76efe3f",
+          "message": "Merge pull request #2 from jinglinpeng/ci3\n\nworkflows",
+          "timestamp": "2021-07-13T11:38:06-07:00",
+          "tree_id": "8888a340e9078145755a58cdc95539ac8a1dee55",
+          "url": "https://github.com/jinglinpeng/dataprep/commit/3bb7d6f7ad9b3a95fa0ac3730a4b68a7b76efe3f"
+        },
+        "date": 1626201759023,
+        "tool": "pytest",
+        "benches": [
+          {
+            "name": "dataprep/tests/benchmarks/eda.py::test_create_report",
+            "value": 0.0745636874085797,
+            "unit": "iter/sec",
+            "range": "stddev: 0.48571844870370534",
+            "extra": "mean: 13.411353901000002 sec\nrounds: 5"
           }
         ]
       }
